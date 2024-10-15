346a391163959f91f0ba297437801e03b29374b7
sinnosuke2701 <ehgml1111@naver.com>
2024-10-15 15:29:18 +0900

    member

---
+++ pathy/src/main/java/com/pathy/infra/member/MemberDao.java
@@ -0,0 +1,29 @@
+package com.pathy.infra.member;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberDao {
+	
+	public int selectOneCount(MemberVo memberVo);
+	
+	List<MemberDto> selectList(MemberVo memberVo);
+	
+	public int insert(MemberDto memberDto);
+	
+	public MemberDto selectOne(MemberDto memberDto);
+	
+	public int update(MemberDto memberDto);
+	
+	public int delete(MemberDto memberDto);
+
+	public int uelete(MemberDto memberDto);
+	
+	public MemberDto selectOneId(MemberDto memberDto);
+	
+	public MemberDto selectOneLogin(MemberDto memberDto);
+	
+	public int insertLogLogin(MemberDto memberDto);
+}
---
+++ pathy/src/main/java/com/pathy/infra/member/MemberService.java
@@ -0,0 +1,67 @@
+package com.pathy.infra.member;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+	
+	@Autowired
+	private MemberDao MemberDao;
+	
+	public int selectOneCount(MemberVo memberVo) {
+		return MemberDao.selectOneCount(memberVo);
+	}
+	
+	List<MemberDto> selectList(MemberVo memberVo){
+		List<MemberDto> members = MemberDao.selectList(memberVo);
+			return members;
+		}
+	
+	public int insert(MemberDto memberDto) {
+		int result = MemberDao.insert(memberDto);
+		return result;
+	}
+	
+	public MemberDto selectOne(MemberDto memberDto) {
+		return MemberDao.selectOne(memberDto);
+	}
+	
+//	public MemberDto selectOne(MemberDto memberDto) {
+//		MemberDto dto = MemberDao.selectOne(memberDto);
+//		return dto;
+//	}
+	
+//	public int update(MemberDto memberDto) {
+//		int a = MemberDao.update(memberDto);
+//		return a;
+//	}
+	
+	public int update(MemberDto memberDto) {
+		return MemberDao.update(memberDto);
+	}
+	
+	public int delete(MemberDto memberDto) {
+		return MemberDao.delete(memberDto);
+	}
+	
+	public int uelete(MemberDto memberDto) {
+		return MemberDao.uelete(memberDto);
+	}
+	
+	public MemberDto selectOneId(MemberDto memberDto) {
+		return MemberDao.selectOneId(memberDto);
+	}
+	
+	public MemberDto selectOneLogin(MemberDto memberDto) {
+		return MemberDao.selectOneLogin(memberDto);
+	}
+	
+	public int insertLogLogin(MemberDto memberDto) {
+		return MemberDao.insertLogLogin(memberDto);
+		
+	}
+	
+}
