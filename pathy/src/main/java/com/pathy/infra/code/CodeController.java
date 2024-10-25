56a493e9b8d017a59a07a6f07e50165287c5de56
mizayeyo <b.mizayeyo@gmail.com>
2024-10-25 15:15:43 +0900

    dd

--- pathy/src/main/java/com/pathy/infra/code/CodeController.java
+++ pathy/src/main/java/com/pathy/infra/code/CodeController.java
@@ -20,17 +20,39 @@ public class CodeController {
 	@RequestMapping(value="/xdm/v1/infra/code/codeXdmList")
 	public String codeXdmList(@ModelAttribute("vo") CodeVo codeVo,Model model){
 		
+		//날짜 필드에 시간 추가
+//		codeVo.setShDateStart(codeVo.getShDateStart()+" 00:00:00");
+//		codeVo.setShDateEnd(codeVo.getShDateEnd()+" 23:59:59");
+		
 		// 초기값 세팅이 없는 경우 사용
-		codeVo.setShDateStart(codeVo.getShDateStart() == null || codeVo.getShDateStart() == "" ? null : UtilDateTime.add00TimeString(codeVo.getShDateStart()));
-		codeVo.setShDateEnd(codeVo.getShDateEnd() == null || codeVo.getShDateEnd() == "" ? null : UtilDateTime.add59TimeString(codeVo.getShDateEnd()));
+				codeVo.setShDateStart(codeVo.getShDateStart() == null || codeVo.getShDateStart() == "" ? null : UtilDateTime.add00TimeString(codeVo.getShDateStart()));
+				codeVo.setShDateEnd(codeVo.getShDateEnd() == null || codeVo.getShDateEnd() == "" ? null : UtilDateTime.add59TimeString(codeVo.getShDateEnd()));
+		
 		
 		// paging
 		codeVo.setParamsPaging(codeService.selectOneCount(codeVo));
 		
 		if (codeVo.getTotalRows() > 0) {
 			model.addAttribute("list", codeService.selectList(codeVo));
+//		    model.addAttribute("vo", codeVo);
 		}
+
 		return "/xdm/v1/infra/code/codeXdmList";
+		
+		
+//		for(CodeDto codedto : codeDto) {
+//			System.out.print(codedto.getIfcdSeq() + " | ");
+//			System.out.print(codedto.getIfcdName() + " | ");
+//			System.out.print(codedto.getIfcdUseNy() + " | ");
+//			System.out.print(codedto.getIfcdOrder() + " | ");
+//			System.out.print(codedto.getIfcdDesc() + " | ");
+//			System.out.print(codedto.getIfcdDelNy() + " | ");
+//			System.out.print(codedto.getIfcdRegist() + " | ");
+//			System.out.print(codedto.getIfcdEdit() + " | ");
+//			System.out.println(codedto.getCodegroupIfcgSeq() + " | ");
+//		}	
+//		return "/xdm/v1/infra/code/codeXdmList";	
+//			
 		}
 	
 	// Form
@@ -88,6 +110,10 @@ public class CodeController {
 			codeService.delete(codeDto);
 			return "redirect:/xdm/v1/infra/code/codeXdmList";
 		}
+		
+	
+
+		
 }
 
 
